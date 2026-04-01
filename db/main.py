78c0a5e3bde8764f64ae2f771afac4e320b4dc69
Hilalova-Malohat <malochat5@gmail.com>
2026-04-01 17:46:52 +0500

    Test3

--- db/main.py
+++ db/main.py
@@ -1,15 +1,10 @@
-import psycopg2
-import os
-from dotenv import load_dotenv
+from db import get_connection
 
-load_dotenv()
+conn = get_connection()
+cur = conn.cursor()
 
-def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
-print(os.getenv("DB_NAME"))
+cur.execute("SELECT * FROM actor LIMIT 5;")
+print(cur.fetchall())
+
+cur.close()
+conn.close()
