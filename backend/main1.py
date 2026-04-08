70e5f0370965f7f67dc72cfa06893897fb1a2002
Medvedeva-Karine <karina.macawoy@gmail.com>
2026-04-08 04:50:59 +0000

    backend/main1.py

--- backend/main1.py
+++ backend/main1.py
@@ -13,21 +13,21 @@ def get_films_by_search(text):
 
         result = []
         for film in films:
-            info = db.get_film_info(conn, film["film_id"])
+            info = db.get_film_info(conn, film["movie_id"])
 
             result.append({
-                "id": film["film_id"],
+                "id": film["movie_id"],
                 "title": film["title"],
                 "description": info["description"] if info else "",
                 "year": info["release_year"] if info else None,
-                "rating": info["rating"] if info else None
+                "imdb_rating": info["imdb_rating"] if info else None
             })
 
         return result
 
     finally:
         conn.close()
-        
+
 def register_user(user_name, user_password, avatar_url=None):
     conn = db.get_connection()
     try:
@@ -54,17 +54,43 @@ def check_user_login(username, password):
             return None
     finally:
         conn.close()
-        
+
 def get_films_by_genre(genre_name):
     conn = db.get_connection()
     try:
         return db.search_film_with_filters()
     finally:
         conn.close()
-        
+
 def get_films_with_filters(title=None, genre=None, actor=None, year=None, sort_by=None, sort_order="asc"):
     conn = db.get_connection()
     try:
         return db.search_film_with_filters(conn, title=title, genre=genre, actor=actor, year=year, sort_by=sort_by, sort_order=sort_order)
+    finally:
+        conn.close()
+def get_user_profile(user_id):
+    conn = db.get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT id, user_name, avatar_url FROM users_schema.users WHERE id=%s",
+                (user_id,)
+            )
+            user = cur.fetchone()
+
+        if not user:
+            return None
+        seen_films = db.get_films_from_users_list(conn, user_id, 1)   # просмотрено
+        planned_films = db.get_films_from_users_list(conn, user_id, 2)  # запланировано
+
+        return {
+            "id": user[0],
+            "username": user[1],
+            "avatar": user[2],
+
+            "seen": seen_films,
+            "planned": planned_films
+        }
+
     finally:
         conn.close()
